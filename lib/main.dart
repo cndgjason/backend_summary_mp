765c9a0d2dcdae859e4a1827a186acddd75ca2f4
jason <cndgjason@outlook.com>
2022-10-05 22:06:47 +0800

    init project

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/widgets.dart';
+import 'package:mpcore/mpcore.dart';
+import './second_page.dart';
+
+void main() {
+  runApp(MyApp());
+  MPCore().connectToHostChannel();
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MPApp(
+      title: 'MPFlutter Demo',
+      color: Colors.blue,
+      routes: {
+        '/': (context) => MyHomePage(),
+        '/second': (context) => MySecondPage(),
+      },
+      navigatorObservers: [MPCore.getNavigationObserver()],
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MPScaffold(
+      name: 'Template',
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed('/second');
+          },
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.blue,
+            child: Center(
+              child: Text(
+                'Hello, MPFlutter!',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
